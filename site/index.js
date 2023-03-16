2165930916d97d24b707a39e9fb82bad5789fdf0
JJ Seay <greenmajesti@gmail.com>
2023-03-15 20:49:36 -0400

    site mvp
    requires installation of oracledb, express, nodemon, ejs with npm
    also requires oracle xe or instantclient

---
+++ site/index.js
@@ -0,0 +1,42 @@
+// 'npm run devStart' in console to run
+// runs on localhost:3000
+// requires installation of oracledb, express, nodemon, ejs
+// I also installed Oracle XE 21 and had to set ORACLE_HOME & other environment variables
+
+// oracle setup
+const oracledb = require('oracledb');
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+// express setup
+const express = require('express')
+const app = express()
+
+async function database_get() {
+	let con;
+	try {
+		con = await oracledb.getConnection({
+			user :          "urnseay.jlindsey",
+			password :      "JNr0t4tK7tiY21aHXEKkivb5",
+			connectString : "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.cise.ufl.edu)(PORT=1521))(CONNECT_DATA=(SID=orcl)))"
+		});
+		const data = await con.execute(
+			'SELECT * FROM COUNTRY WHERE ROWNUM <= 5',
+		);
+		//console.log(data);
+		return data.rows;
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+app.set('view engine', 'ejs')
+app.get('/', (request, response) => {
+	// fetches the data, then sends it to be rendered
+	// renders index.ejs
+	database_get()
+	.then(function(data) {
+		response.render('index', {mydata: data})
+	})
+})
+
+app.listen(3000)
